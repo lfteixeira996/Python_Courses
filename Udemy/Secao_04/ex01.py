15b48fec8d5d540f0f212d6e209186cc59fe5d4c
Luis Teixeira <filipe.teixeira.996@gmail.com>
2021-02-25 23:22:30 +0000

    git cache cleared

--- Udemy/Secao_04/ex01.py
+++
@@ -1,7 +0,0 @@
-"""
-Faça um program que leia um numero inteiro e o imprima
-"""
-
-val_str = input("Insira um valor inteiro: ") 
-
-print(f"Valor inserido: {int(val_str)}")
--- Udemy/Secao_04/ex02.py
+++
@@ -1,7 +0,0 @@
-"""
-Faça um program que leia um numero real e o imprima
-"""
-
-val_str = input("Insira um valor inteiro: ") 
-
-print(f"Valor inserido: {float(val_str)}")
