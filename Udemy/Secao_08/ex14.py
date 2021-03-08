a04d789eed672722c8be3aec41005ae36af67afd
Luis Teixeira <filipe.teixeira.996@gmail.com>
2021-03-08 18:00:48 +0000

    new exercises added

--- Udemy/Secao_08/ex14.py
+++ Udemy/Secao_08/ex14.py
@@ -15,7 +15,12 @@ percurso, calcule o consumo em km/l e escreva uma uma mensagem de acordo com a t
 
 
 def km_l(km, litros):
-
+    """
+    Função
+    :param km: kilometros
+    :param litros: combustivel consumido
+    :return: combustivel consumido por km
+    """
     km_litros = km/litros
 
     if km_litros < 8:
